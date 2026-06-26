99080df0a1e81a5486f1b8ed7d197c7dc7704f5f
peipei2015 <1607458232@qq.com>
2026-06-26 23:07:24 +0800

    Update count.php

--- count.php
+++ count.php
@@ -1,10 +1,18 @@
 <?php
-$txt = "count.txt";
-if (!file_exists($txt)) {
-    file_put_contents($txt, 0);
+// 计数存储文件
+$countFile = "count.txt";
+
+// 文件不存在则创建，初始值 0
+if (!file_exists($countFile)) {
+    file_put_contents($countFile, "0");
 }
-$c = (int)file_get_contents($txt);
-$c++;
-file_put_contents($txt, $c);
-echo $c;
+
+// 读取当前次数
+$total = (int)file_get_contents($countFile);
+$total++;
+// 写入新次数
+file_put_contents($countFile, $total);
+
+// 输出最新访问总数给网页
+echo $total;
 ?>
